9154b14d66dc55b9a782b3a7d95098654f3dffac
Natasha Johnson <johnsonn@si.edu>
2022-08-09 16:10:09 -0400

    Just pushing up what I have before I leave.

--- drupal-site/nextjs-app/stories/components/Main--header.tsx
+++ drupal-site/nextjs-app/stories/components/Main--header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from "next/link"
 import { DrupalMenuLinkContent } from "next-drupal"
+import { useRouter } from "next/router"
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'   
 
