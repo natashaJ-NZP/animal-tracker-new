b6e0f6f576d382019a33ad603f6ed59fa6e49f82
Natasha Johnson <johnsonn@si.edu>
2022-08-09 14:21:28 -0400

    Okay heres what i did today so far: first I went into the drupal side and created a menu there, then i went into the nextjs app and coded in the logic to make the drupal menu to appear. Lastly, i went back into the drupal side to download the json api menu and json api resources modules so that drupalrecognizes the nextjs logic to fetch data for the drupal menu i just created.  Now im testing it out.

--- drupal-site/nextjs-app/pages/index.tsx
+++ drupal-site/nextjs-app/pages/index.tsx
@@ -4,15 +4,33 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { useSession } from "next-auth/react"
 import { useRouter } from "next/router"
+import {
+  DrupalBlock,
+  DrupalMenuLinkContent,
+  DrupalTaxonomyTerm,
+} from "next-drupal"
 
 //components
 import { Header } from "../stories/components/Main--header"
 import { Footer } from "../stories/components/Footer"
 
-const NextPage = () => {
+export interface HeaderProps {
+  menus: {
+    header: DrupalMenuLinkContent[]
+  }
+  blocks: {
+    recipeCollections: DrupalTaxonomyTerm[]
+    footerPromo: DrupalBlock
+    disclaimer: DrupalBlock
+  }
+}
+
+const NextPage = ({menus, blocks}: HeaderProps) => {
   return (
     <div>
-      <Header />
+      {menus?.header?.length ? (
+        <Header items={menus.header}/>
+      ) : null}
       <div>You are now on the homepage.</div>
       <Footer />
     </div>
